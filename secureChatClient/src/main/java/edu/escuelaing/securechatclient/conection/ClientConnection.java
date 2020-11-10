dc70bccd50a6a8be68ecbd41869d17a2458ff1d9
diegofch29 <diegofch29@hotmail.com>
2020-11-10 14:23:20 -0500

    Ip actualizada martes

--- secureChatClient/src/main/java/edu/escuelaing/securechatclient/conection/ClientConnection.java
+++ secureChatClient/src/main/java/edu/escuelaing/securechatclient/conection/ClientConnection.java
@@ -35,7 +35,7 @@ public class ClientConnection {
         
         
         try {
-            echoSocket = new Socket("127.0.0.1", 35000);
+            echoSocket = new Socket("54.196.149.99", 35000);
             out = new PrintWriter(echoSocket.getOutputStream(), true); in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
             in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
         } catch (UnknownHostException e) {
